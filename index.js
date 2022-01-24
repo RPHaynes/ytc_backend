ca13aa310bdc9d341ea23987a268357c96e2eced
rphaynes <ryan.haynes@revature.net>
2022-01-24 11:36:09 -0500

    test

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 //import express
 import express from "express";
+import UserDAO from "./DAOs/userDAO.js";
+import Client from "./utils/dbinfo.js";
 //create application
 const app = express();
 //define port
 const port = 3000;
+const userdao = new UserDAO();
+await userdao.open();
+await userdao.createTableIfNotExist();
 
 //define route as root for a get request
-app.get("/", (req, res) => {
-	res.send("Hello World!");
+app.get("/", async (req, res) => {
+	res.send(await (await userdao.getTables()).rows);
 });
 
 //listen on port
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
-
-export default app;
